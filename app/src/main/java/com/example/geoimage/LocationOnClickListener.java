1308a353ef094ea0d9db13cc98c3629194f6a529
Sylvain Leclercq <maisbiensurqueoui@gmail.com>
2023-05-20 12:05:33 +0200

    Add geolocation button

---
+++ app/src/main/java/com/example/geoimage/LocationOnClickListener.java
@@ -0,0 +1,71 @@
+package com.example.geoimage;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+
+public class LocationOnClickListener implements View.OnClickListener {
+    private final MainActivity mainActivity;
+
+    public LocationOnClickListener(MainActivity mainActivity) {
+        this.mainActivity = mainActivity;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (ContextCompat.checkSelfPermission(mainActivity,
+                                              android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(mainActivity,
+                                                                                       android.Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+
+            LocationManager locationManager =
+                    (LocationManager) mainActivity.getSystemService(Context.LOCATION_SERVICE);
+
+            // Check if GPS_PROVIDER is enabled on the device
+            if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                Log.i("CIO", "GPS Provider is not enabled");
+                return;
+            }
+
+            Button locationButton = mainActivity.findViewById(R.id.get_coordinates_button);
+            locationButton.setEnabled(false);
+            // Request location updates
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(@NonNull Location location) {
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    TextView latitudeTextView = mainActivity.findViewById(R.id.lat);
+                    TextView longitudeTextView = mainActivity.findViewById(R.id.lon);
+                    latitudeTextView.setText(String.valueOf(latitude));
+                    longitudeTextView.setText(String.valueOf(longitude));
+                    locationButton.setEnabled(true);
+                    locationManager.removeUpdates(this);
+                }
+
+                // Add other required methods for the LocationListener interface...
+            });
+        } else {
+            Log.i("CIO", "Permissions denied. SDK_INT=" + Build.VERSION.SDK_INT);
+            Log.i("CIO", "ACCESS_COARSE_LOCATION: " +
+                    ContextCompat.checkSelfPermission(mainActivity,
+                                                      android.Manifest.permission.ACCESS_COARSE_LOCATION));
+            Log.i("CIO", "ACCESS_FINE_LOCATION: " + ContextCompat.checkSelfPermission(mainActivity,
+                                                                                      android.Manifest.permission.ACCESS_FINE_LOCATION));
+            Log.i("CIO", "PackageManager.PERMISSION_GRANTED=" + PackageManager.PERMISSION_GRANTED);
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/geoimage/MainActivity.java
+++ app/src/main/java/com/example/geoimage/MainActivity.java
@@ -1,14 +1,112 @@
 package com.example.geoimage;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import org.jetbrains.annotations.Contract;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Permissions permissions = Permissions.None;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button locationButton = findViewById(R.id.get_coordinates_button);
+        locationButton.setEnabled(false);
+
+        if (ContextCompat.checkSelfPermission(this,
+                                              android.Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                                              new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                                              Permissions.Coarse.value);
+
+        } else {
+            this.permissions = this.permissions.and(Permissions.Coarse);
+            requestFineLocationPermission();  // If Coarse permission is already granted, request for Fine
+        }
+    }
+
+    private void requestFineLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                                              android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                                              new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                                              Permissions.Fine.value);
+        } else {
+            this.permissions = this.permissions.and(Permissions.Fine);
+            activateButton();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Permissions permission = Permissions.from(requestCode);
+        Log.i("CIO", "onRequestPermissionsResult: " + permission);
+        Log.i("CIO", "requestCode: " + requestCode);
+
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            this.permissions = this.permissions.and(permission);
+            activateButton();
+        }
+
+        // If Coarse permission is granted, request for Fine
+        if (permission == Permissions.Coarse && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            requestFineLocationPermission();
+        }
+    }
+
+    private void activateButton() {
+        if (this.permissions.bothActivated()) {
+            Button locationButton = findViewById(R.id.get_coordinates_button);
+            locationButton.setEnabled(true);
+            locationButton.setOnClickListener(new LocationOnClickListener(this));
+        }
+    }
+
+    public enum Permissions {
+        None(0b00), Coarse(0b01), Fine(0b10), Both(0b11);
+
+        private final int value;
+
+        Permissions(int value) {
+            this.value = value;
+        }
+
+        public boolean bothActivated() {
+            return value == Both.value;
+        }
+
+        @Contract(pure = true)
+        public Permissions and(@NonNull Permissions other) {
+            return Permissions.from(value | other.value);
+        }
+
+        public static Permissions from(int value) {
+            int perm = value & 0b11;
+            switch (perm) {
+                case 0b01:
+                    return Coarse;
+                case 0b10:
+                    return Fine;
+                case 0b11:
+                    return Both;
+                default:
+                    return None;
+            }
+        }
     }
 }
